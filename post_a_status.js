666369c14d84e43015e18d10eaf065f8c91797b9
William McCann <45@holbertonschool.com>
2016-02-26 21:08:49 -0800

    rough outline of JS

--- post_a_status.js
+++ post_a_status.js
@@ -1,16 +1,42 @@
+Definitely need to ensure page is loaded before everything, per spec (onload maybe?)
+Use an event listener to hear button click
+
+/*this function is triggered by the onLoad function at the bottom, and
+contains all the other functions for the page*/
+function setUpEvents(){
+
+    /*adds event listeners to listen for the button clicks in the HTML*/
+    function addListeners(){
+        if(window.addEventListener) {
+            document.getElementById('BUTTON ID');.addEventListener("click",BUTTON LISTENER FUNCTION NAME,false);
+        }   else if (window.attachEvent) {
+                document.getElementById('BUTTON LISTENER FUNCTION NAME').attachEvent("onclick", BUTTON LISTENER FUNCTION NAME);
+        }
+        function BUTTON LISTENER FUNCTION NAME(){
+            FUNCTION THAT DOES THE THING YOU WANT TO DO;
+        }
+    };
 
 
 
-    <html>
-    <body onload="myFunction()">
 
-    <h1>Hello World!</h1>
 
-    <script>
-    function myFunction() {
-	alert("Page is loaded");
-    }
-</script>
 
-</body>
-    </html>
+
+}
+
+/*this function triggers the function at the top and ensures
+all of the JS loads after the HTML loads*/
+window.onload = function(){
+    
+        setUpEvents();
+    
+};
+
+
+
+
+
+
+var form = document.getElementById("IIIIDDDDD").style. NOOOTTTINVISIBLE
+
