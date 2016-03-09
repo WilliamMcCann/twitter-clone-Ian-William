9ea81e2e1462e40520599a47e6da74d8f474640f
IanWagener <15@holbertonschool.com>
2016-03-09 14:38:09 -0800

    fixed html and JS errors

--- reply.js
+++ reply.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
-    var total = document.getElementsByClassName("buttonreply");
+    var buttontotal = document.getElementsByClassName("buttonreply");
     for (i = 0; i < total.length; i++){
         total[i].addEventListener("click", function(){
          var buttons = document.getElementById(this.dataset.order);
